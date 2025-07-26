913bbaf879d65f4c070e8c2f9e9c859bbb24065b
Bishal Adhikary <bishaladhikaryreal@gmail.com>
2025-07-26 20:13:44 +0530

    LogMonitorAppV3 Design Phase

---
+++ LogMonitorAppV3/src/test/java/com/bishal/logmonitorappv3/LogMonitorAppV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bishal.logmonitorappv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogMonitorAppV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
