09e7e9f03c1f8deeb27c368cd351ecc5c0a7bab6
Bishal Adhikary <bishaladhikaryreal@gmail.com>
2025-08-15 09:59:21 +0530

    credential's format checker method added

--- ReceipeAppBackend/src/main/java/com/bishal/receipeappbackend/core/authenticator/Authenticator.java
+++ ReceipeAppBackend/src/main/java/com/bishal/receipeappbackend/core/authenticator/Authenticator.java
@@ -1,14 +1,35 @@
 package com.bishal.receipeappbackend.core.authenticator;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Authenticator {
 
 
+	public static void main(String... args)
+	{
+
+		String username = "bishal";
+		String password = "3%";
+		System.out.println("Authentication of username: "+username+" and password: "+ password +"\n"  + authenticate(username,password));
+
+
+		Pattern passwordPattern = Pattern.compile("([a-zA-Z!@#$%^&*()])*([0-9])+([a-zA-Z!@#$%^&*()])*([a-zA-Z0-9])*([!@#$%^&*()])+([a-zA-Z0-9])*|([a-zA-Z0-9])*([!@#$%^&*()])+([a-zA-Z0-9])*([a-zA-Z!@#$%^&*()])*([0-9])+([a-zA-Z!@#$%^&*()])*");
+		Matcher m = passwordPattern.matcher(password);
+
+		System.out.println("Password Matches: " + m.matches());
+	}
+
 	public static boolean authenticate(String username, String password)
 	{
 
 
+		if(checkCredentialFormat(username, password))
+		{
+			return true;
+		}
+
+		return false;
 
 
 
@@ -16,16 +37,29 @@ public class Authenticator {
 	}
 
 
-	public static boolean credentialChecker(String username, String password)
+	public static boolean checkCredentialFormat(String username, String password)
 	{
-		Pattern usernamePattern = Pattern.compile("([a-zA-Z0-9]{1,20})");
+		username = username.trim();
+		password = password.trim();
 
-		Pattern passwordPattern = Pattern.compile("([a-zA-Z!@#$%^&*()])*([0-9])+([a-zA-Z!@#$%^&*()])*([a-zA-Z0-9])*([!@#$%^&*()])+([a-zA-Z0-9])*|([a-zA-Z0-9])*([!@#$%^&*()])+([a-zA-Z0-9])*([a-zA-Z!@#$%^&*()])*([0-9])+([a-zA-Z!@#$%^&*()])*");
+		if(username.length()>=5 && username.length() <= 20 && password.length()>=5 && password.length()<=20)
+		{
+			Pattern usernamePattern = Pattern.compile("([a-zA-Z0-9]{1,20})");
+
+			Pattern passwordPattern = Pattern.compile("([a-zA-Z!@#$%^&*()])*([0-9])+([a-zA-Z!@#$%^&*()])*([a-zA-Z0-9])*([!@#$%^&*()])+([a-zA-Z0-9])*|([a-zA-Z0-9])*([!@#$%^&*()])+([a-zA-Z0-9])*([a-zA-Z!@#$%^&*()])*([0-9])+([a-zA-Z!@#$%^&*()])*");
 
 
-		String p =""
+			Matcher uMatcher = usernamePattern.matcher(username);
+			Matcher pMatcher = passwordPattern.matcher(password);
 
+			if(uMatcher.matches() && pMatcher.matches() )
+			{
+				return true;
+			}
 
+		}
+
+		return false;
 	}
 
 
