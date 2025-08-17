fe68e92a6abd0e167c1218bb00abac6b53c94422
Bishal Adhikary <bishaladhikaryreal@gmail.com>
2025-08-17 13:58:01 +0530

    regUser api created UNTESTED

--- ReceipeAppBackend/src/main/java/com/bishal/receipeappbackend/core/controller/Controller.java
+++ ReceipeAppBackend/src/main/java/com/bishal/receipeappbackend/core/controller/Controller.java
@@ -2,8 +2,10 @@ package com.bishal.receipeappbackend.core.controller;
 
 
 import com.bishal.receipeappbackend.core.authenticator.ParallelAuthenticator;
+import com.bishal.receipeappbackend.core.authenticator.ParallelRegistrator;
 import com.bishal.receipeappbackend.core.dbhandler.DbHandler;
 import com.bishal.receipeappbackend.core.model.AuthRequestDTO;
+import com.bishal.receipeappbackend.core.model.RegRequestDTO;
 import com.bishal.receipeappbackend.core.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,12 +19,14 @@ import java.util.concurrent.Future;
 public class Controller {
 
 	private final ParallelAuthenticator authenticator;
+	private final ParallelRegistrator pr;
 
 
 	@Autowired
-	public Controller(ParallelAuthenticator pa)
+	public Controller(ParallelAuthenticator pa,ParallelRegistrator pr)
 	{
 		this.authenticator=pa;
+		this.pr=pr;
 	}
 
 
@@ -38,5 +42,11 @@ public class Controller {
 
 		return DbHandler.runQuery(request.getUsername(),request.getPassword(),DbHandler.fetchUserFunction);
 
+	}
+
+	@PostMapping("/regUser")
+	public CompletableFuture<Boolean> regUser(@RequestBody RegRequestDTO regRequest)
+	{
+		return  pr.registerUser(regRequest);
 	}
 }
